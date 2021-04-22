eb280d867912f30ad5a999bd30efc28a667f1665
Yohannes Berhanu <Johnlights51@gmail.com>
2021-04-22 17:52:58 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="bg-gray-100">
+      <header className="App-header min-h-screen bg-gray-500">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
